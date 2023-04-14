b891ff5fcfab057549fb12367d92ce4ae1711415
Jatin Undhad <jatinundhad33@gmail.com>
2023-04-14 10:03:22 +0530

    simulation done

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,25 +1,48 @@
-var liquid = document.getElementById('liquid');
-// liquid.style.fill="orange";
+let titrantUsed = 0;
+var liquid = document.getElementById("liquid");
+// liquid.style.fill = "orange";
 
-var buretteLiquid=document.getElementById('buretteLiquid');
-buretteLiquid.classList.remove('buretteAnimation')
+var buretteLiquid = document.getElementById("buretteLiquid");
+buretteLiquid.classList.remove("buretteAnimation");
 
-var drip=document.getElementById('drip');
+var drip = document.getElementById("drip");
 console.log(drip);
 if (drip.getAttribute("dur") === null) {
-    drip.setAttribute("dur", "0s");
-} 
-else {
+  drip.setAttribute("dur", "0s");
+} else {
+  drip.removeAttribute("dur");
+}
+
+function startdrip() {
+  let colorchange = (Math.random() * 50).toFixed(1);
+  colorchange = parseFloat(colorchange);
+  console.log(colorchange);
+  // speed=1;
+
+  let intervalID = setInterval(() => {
+    titrantUsed = parseFloat(titrantUsed) + 0.1;
+    titrantUsed = parseFloat(titrantUsed).toFixed(1);
+    document.getElementById("titrant-used").innerHTML =
+      "Titrant Used : " + titrantUsed + "ml";
+    if (titrantUsed == colorchange) {
+      liquid.style.fill = "purple";
+      drip.removeAttribute("dur");
+      buretteLiquid.style.animationPlayState = "paused";
+      clearInterval(intervalID);
+    }
+  }, 100);
+  buretteLiquid.classList.add("buretteAnimation");
+
+  if (drip.getAttribute("dur") === null) {
+    drip.setAttribute("dur", "1s");
+  } else {
     drip.removeAttribute("dur");
+  }
 }
 
-function startdrip(){
-    buretteLiquid.classList.add('buretteAnimation')
-    if (drip.getAttribute("dur") === null) {
-        drip.setAttribute("dur", "1s");
-      } 
-    //   else {
-    //     drip.removeAttribute("dur");
-    //   }
-}
-
+document.querySelectorAll(".nav-link").forEach((item) => {
+  item.addEventListener("click", (e) => {
+    document.querySelector(".active").classList.remove("active");
+    item.classList.add("active");
+  });
+});
